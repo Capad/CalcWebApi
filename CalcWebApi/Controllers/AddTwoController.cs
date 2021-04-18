8138b5bb5d646b371c7d1176e91015618474b1c7
Aleksy Kabat <kabatdev@gmail.com>
2021-04-18 22:21:47 +0200

    Adding basic operations for two variables

---
+++ CalcWebApi/Controllers/AddTwoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CalcWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddTwoController : ControllerBase
+    {
+        private readonly ILogger<AddTwoController> _logger;
+        public AddTwoController(ILogger<AddTwoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public double Get(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ CalcWebApi/Controllers/MultiplyTwoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CalcWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MultiplyTwoController : ControllerBase
+    {
+        private readonly ILogger<MultiplyTwoController> _logger;
+        public MultiplyTwoController(ILogger<MultiplyTwoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public double Get(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
